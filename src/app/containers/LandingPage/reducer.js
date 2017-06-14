17a71f69ef98469cd82bdb36f81c1743dce564fb
Mahima Agrawal <mahi9.goyal@gmail.com>
2017-06-14 17:05:13 +0530

    Added lodash deep clone method to clone state in reducer

--- src/app/containers/LandingPage/reducer.js
+++ src/app/containers/LandingPage/reducer.js
@@ -2,32 +2,32 @@
  * Created by intelligrape on 6/6/17.
  */
 import {LoginActions} from './constants';
+var _ = require('lodash');
+
 const initialState = {
     login: {},
     isFetching: true
 };
 
 const landingPageReducer = (state = initialState, action) => {
+    let duplicateState = _.cloneDeep(state);
     switch(action.type) {
         case LoginActions.LoginSuccess:
-            return {
-                ...state,
-                login: {
-                    isLogin: true,
-                    data: action.data,
-                }
+            duplicateState.login = {
+                isLogin: true,
+                data: action.data,
             };
+            break;
         case LoginActions.LoginFailure:
-            return {
-                ...state,
-                login: {
-                    isLogin: false,
-                    error:action.error.message
-                }
+            duplicateState.login = {
+                isLogin: false,
+                error:action.error.message
             };
+            break;
         default:
-            return state;
+            break;
     }
+    return duplicateState;
 };
 
 export default landingPageReducer;
